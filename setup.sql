0365c35e564d243498b1f6cae2becfc3479c903d
Okao <hamzathanees@gmail.com>
2023-08-01 09:17:25 +0500

    DB Connection via Config

--- setup.sql
+++ setup.sql
@@ -4,3 +4,16 @@ CREATE TABLE IF NOT EXISTS `books_reviews` (
   `book_review` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- create table for book users if not exists
+CREATE TABLE IF NOT EXISTS `book_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- insert data into book users table
+INSERT INTO `book_users` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin'),
+(2, 'user', 'user');
