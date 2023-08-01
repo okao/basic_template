be26ba8938d9f7c311938e054faec60483037030
Okao <hamzathanees@gmail.com>
2023-08-01 16:36:02 +0500

    Outlet

--- server/controllers/book_controller.js
+++ server/controllers/book_controller.js
@@ -84,7 +84,7 @@ const update_book = async (req, res) => {
   try {
     // const result = await db.promise().query(UpdateQuery, [bookReview, bookId]);
 
-    const result = await database_sequelize.sequelize.query(UpdateQuery, {
+    const result = await database.sequelize.query(UpdateQuery, {
       type: QueryTypes.UPDATE,
       plain: false,
       raw: true,
@@ -106,14 +106,12 @@ const delete_book = async (req, res) => {
   try {
     // const result = await db.promise().query(DeleteQuery, bookId);
 
-    const result = await database_sequelize.sequelize.query(DeleteQuery, {
+    const result = await database.sequelize.query(DeleteQuery, {
       type: QueryTypes.DELETE,
       plain: false,
       raw: true,
       replacements: [bookId],
     });
-
-    console.log(result);
 
     return res.send("Values Deleted");
   } catch (err) {
